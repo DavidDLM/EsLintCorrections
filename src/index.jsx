d919badfb14516475e7a8a21a0b9d8e1b13f90bf
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2022-04-23 03:41:39 -0600

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import './components/Card.jsx';
+
+render(<App />, document.getElementById('root'));
